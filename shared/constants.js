1428ae88210edc9f2617704f927b9e6db3d450d8
Felix Guo <felixguoxiuping@gmail.com>
2019-05-17 15:37:24 -0400

    lumen: change gold for prod games

--- shared/constants.js
+++ shared/constants.js
@@ -1,10 +1,11 @@
+
 module.exports = {
     RED_SIDE: 'red',
     BLUE_SIDE: 'blue',
     RED_CHAT_COLOR: 'red',
     BLUE_CHAT_COLOR: 'dodgerblue',
     NONE_SIDE: 'none',
-    STARTING_GOLD: 10000,
+    STARTING_GOLD: process.env.NODE_ENV === 'production' ? 1000 : 10000,
     BUILD_RANGE: 4,
     BUILDING_VISION_RANGE: 5,
     TIME_IN_SECONDS_BEFORE_GAME_START: 3
