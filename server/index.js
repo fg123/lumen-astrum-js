4e5fc3059797ff9cfb4868c0a5658d9ba83be35b
NotNickKo <nick.ko123@gmail.com>
2020-11-20 22:47:21 -0800

    Added 2 maps to map pool

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ function startServer() {
 
     const queues = {
         '4p': new Queue(4, ['4p']),
-        '2p': new Queue(2, ['2p-duel']),
+        '2p': new Queue(2, ['2p-duel', '2p-anchor', '2p-cornucopia']),
         '3p': new Queue(3, ['3p']),
         '2v2': new Queue(4, ['2v2']),
         'pve': new Queue(1, ['pve'])
